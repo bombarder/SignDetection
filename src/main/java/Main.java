70964ac6e7adbc963655f1cca429b35e97583d74
bombarder <makarovmykola@gmail.com>
2018-09-09 20:45:20 +0300

    Added second xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ import org.opencv.core.Core;
 
 public class Main {
     public static void main(String[] args) {
-        // Load the native OpenCV library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new ObjectDetection().run(args);
+        new ObjectDetection().run();
     }
 }
--- src/main/java/ObjectDetection.java
+++ src/main/java/ObjectDetection.java
@@ -7,54 +7,59 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 
 class ObjectDetection {
-    private void detectAndDisplay(Mat frame, CascadeClassifier signCascade) {
+
+
+    private void detectAndDisplay(Mat frame,  CascadeClassifier pedestrianCascade, CascadeClassifier stopCascade) {
         Mat frameGray = new Mat();
         Imgproc.cvtColor(frame, frameGray, Imgproc.COLOR_BGR2GRAY);
         Imgproc.equalizeHist(frameGray, frameGray);
 
         // -- Detect pedestrian sign
         MatOfRect pedestrianSign = new MatOfRect();
-        signCascade.detectMultiScale(frameGray, pedestrianSign);
+        pedestrianCascade.detectMultiScale(frameGray, pedestrianSign);
 
-        List<Rect> listOfSigns = pedestrianSign.toList();
-        for (Rect sign : listOfSigns) {
+        MatOfRect stopSign = new MatOfRect();
+        stopCascade.detectMultiScale(frameGray, stopSign);
+
+        List<Rect> listOfPedestrianSigns = pedestrianSign.toList();
+        for (Rect sign : listOfPedestrianSigns) {
             Point rightDownAngle = new Point(sign.x + sign.width, sign.y + sign.height);
             Point leftUpAngle = new Point(sign.x, sign.y);
             Point leftDownAngle = new Point(sign.x, sign.y + sign.height + 20);
-            Imgproc.rectangle(frame, rightDownAngle,  leftUpAngle, new Scalar(255, 0, 0), 4);
-            Imgproc.putText(frame,"pedestrian", leftDownAngle, Core.FONT_HERSHEY_SIMPLEX, 1.0, new Scalar(0, 0, 255),
-                    2, 2,false);
+            Imgproc.rectangle(frame, rightDownAngle, leftUpAngle, new Scalar(255, 0, 0), 4);
+            Imgproc.putText(frame, "pedestrian", leftDownAngle, Core.FONT_HERSHEY_SIMPLEX, 1.0, new Scalar(0, 0, 255),
+                    2, 2, false);
         }
 
-        HighGui.imshow("Capture - Sign detection", frame);
+        List<Rect> listOfTopSigns = stopSign.toList();
+        for (Rect sign : listOfTopSigns) {
+            Point rightDownAngle = new Point(sign.x + sign.width, sign.y + sign.height);
+            Point leftUpAngle = new Point(sign.x, sign.y);
+            Point leftDownAngle = new Point(sign.x, sign.y + sign.height + 20);
+            Imgproc.rectangle(frame, rightDownAngle, leftUpAngle, new Scalar(255, 0, 0), 4);
+            Imgproc.putText(frame, "stop", leftDownAngle, Core.FONT_HERSHEY_SIMPLEX, 1.0, new Scalar(0, 0, 255),
+                    2, 2, false);
+        }
+
+        HighGui.imshow("Capture - Road Sign detection", frame);
     }
 
-    public void run(String[] args) {
-        String filenameFaceCascade = args.length > 2 ? args[0] : "C:/Work/projects/signRecognition/src/main/resources/cascade.xml";
-        int cameraDevice = args.length > 2 ? Integer.parseInt(args[2]) : 0;
+    public void run() {
+        String pedestrianCascade = "C:/Users/user/IdeaProjects/SignDetection/src/main/resources/pedestrianSign.xml";
+        String stopCascade = "C:/Users/user/IdeaProjects/SignDetection/src/main/resources/stopSign.xml";
+        int cameraDevice = 0;
 
-        CascadeClassifier signCascade = new CascadeClassifier();
+        CascadeClassifier pedestrianCascadeClassifier = new CascadeClassifier();
+        pedestrianCascadeClassifier.load(pedestrianCascade);
 
-        if (!signCascade.load(filenameFaceCascade)) {
-            System.err.println("--(!)Error loading sign cascade: " + filenameFaceCascade);
-            System.exit(0);
-        }
+        CascadeClassifier stopCascadeClassifier = new CascadeClassifier();
+        stopCascadeClassifier.load(stopCascade);
 
         VideoCapture capture = new VideoCapture(cameraDevice);
-        if (!capture.isOpened()) {
-            System.err.println("--(!)Error opening video capture");
-            System.exit(0);
-        }
-
         Mat frame = new Mat();
-        while (capture.read(frame)) {
-            if (frame.empty()) {
-                System.err.println("--(!) No captured frame -- Break!");
-                break;
-            }
 
-            //-- 3. Apply the classifier to the frame
-            detectAndDisplay(frame, signCascade);
+        while (capture.read(frame)) {
+            detectAndDisplay(frame, pedestrianCascadeClassifier, stopCascadeClassifier);
 
             if (HighGui.waitKey(10) == 27) {
                 break;// escape

68ea3edc62c6697a5f024591d4b8989a982573c3
Nick <makarovmykola@gmail.com>
2018-09-10 18:38:12 +0300

    implemented quit menu functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,31 +21,36 @@ public class Main {
         availableClassifiers.add(stopCascade);
 //        availableClassifiers.add(stopLight);
 
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Сделайте Ваш выбор:"));
-        int result = JOptionPane.showOptionDialog(null, panel, DIALOG_NAME,
-                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, NAME_OF_BUTTONS, null);
+        while (true){
+            JPanel panel = new JPanel();
+            panel.add(new JLabel("Сделайте Ваш выбор:"));
+            int result = JOptionPane.showOptionDialog(null, panel, DIALOG_NAME,
+                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                    null, NAME_OF_BUTTONS, null);
 
-        if (result == JOptionPane.YES_OPTION) {
-            new SignDetection(availableClassifiers).run();
-        } else if (result == JOptionPane.YES_NO_CANCEL_OPTION) {
+            if (result == JOptionPane.YES_OPTION) {
+                new SignDetection(availableClassifiers).run();
+                break;
+            } else if (result == JOptionPane.YES_NO_CANCEL_OPTION) {
 
-            JPanel innerPanel = new JPanel();
-            ImageIcon pedestrian = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/pedestrian.jpeg");
-            ImageIcon stop = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/stop.jpg");
-            ImageIcon redLight = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/redLight.jpg");
+                JPanel innerPanel = new JPanel();
+                ImageIcon pedestrian = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/pedestrian.jpeg");
+                ImageIcon stop = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/stop.jpg");
+                ImageIcon redLight = new ImageIcon("C:/Work/projects/signRecognition/src/main/resources/redLight.jpg");
 
-            JLabel label1 = new JLabel(pedestrian);
-            JLabel label2 = new JLabel(stop);
-            JLabel label3 = new JLabel(redLight);
+                JLabel label1 = new JLabel(pedestrian);
+                JLabel label2 = new JLabel(stop);
+                JLabel label3 = new JLabel(redLight);
 
-            innerPanel.add(label1);
-            innerPanel.add(label2);
-            innerPanel.add(label3);
+                innerPanel.add(label1);
+                innerPanel.add(label2);
+                innerPanel.add(label3);
 
-            JOptionPane.showMessageDialog(null, innerPanel,
-                    "Модели знаков доступные для определения", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, innerPanel,
+                        "Модели знаков доступные для определения", JOptionPane.PLAIN_MESSAGE);
+            } else {
+                break;
+            }
         }
     }
 }

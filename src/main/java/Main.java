93e8b3a0a7b9dbfdcd51e84449a1ab8baf5cba9c
Nick <makarovmykola@gmail.com>
2018-09-10 11:55:50 +0300

    available two classifiers at the same time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import org.opencv.core.Core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        new ObjectDetection().run();
+        String pedestrianCascade = "C:/Work/projects/signRecognition/src/main/resources/pedestrianSign.xml";
+        String stopCascade = "C:/Work/projects/signRecognition/src/main/resources/stopSign.xml";
+
+        List<String> availableClassifiers = new ArrayList<String>();
+        availableClassifiers.add(pedestrianCascade);
+        availableClassifiers.add(stopCascade);
+
+        new SignDetection(availableClassifiers).run();
     }
 }
---
+++ src/main/java/SignDetection.java
@@ -0,0 +1,48 @@
+import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
+
+import java.util.List;
+
+public class SignDetection {
+
+    private List<String> classifiersList;
+    private int cameraDevice = 0;
+    private VideoCapture capture = new VideoCapture(cameraDevice);
+    private Mat frame = new Mat();
+
+    SignDetection(List<String> classifiersList) {
+        this.classifiersList = classifiersList;
+    }
+
+    public void run() {
+        while (capture.read(frame)) {
+            for (String classifier : classifiersList) {
+                CascadeClassifier cascadeClassifier = new CascadeClassifier();
+                cascadeClassifier.load(classifier);
+                detect(frame, cascadeClassifier);
+            }
+            HighGui.imshow("Road Sign detection", frame);
+            if (HighGui.waitKey(10) == 27) {
+                break;// escape
+            }
+        }
+    }
+
+    private void detect(Mat frame, CascadeClassifier classifier) {
+        Mat frameGray = new Mat();
+        Imgproc.cvtColor(frame, frameGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist(frameGray, frameGray);
+        MatOfRect matOfRect = new MatOfRect();
+        classifier.detectMultiScale(frameGray, matOfRect);
+
+        List<Rect> listOfSigns = matOfRect.toList();
+        for (Rect sign : listOfSigns) {
+            Point rightDownAngle = new Point(sign.x + sign.width, sign.y + sign.height);
+            Point leftUpAngle = new Point(sign.x, sign.y);
+            Imgproc.rectangle(frame, rightDownAngle, leftUpAngle, new Scalar(255, 0, 0), 2);
+        }
+    }
+}
